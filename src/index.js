dd73f34e38598e0fdc174e716fbd414952f67bf1
nicholashutter <nicholas.hutter@live.com>
2021-12-22 18:26:04 -0600

    fixed bug, made winner calculate

--- src/index.js
+++ src/index.js
@@ -35,7 +35,19 @@ function Square(props)
     }
   
     render() {
-      const status = 'Next player: ' +
+      const winner =
+      calculateWinner(this.state.squares);
+      let status;
+      if(winner)
+      {
+        status = 'Winner:  ' + winner;
+      }
+      else
+      {
+        status = 'Next player:  ' +
+        (this.state.xIsNext ? 'X' : 'O');
+      }
+        status = 'Next player: ' +
       (this.state.xIsNext ? 'X' : 'O');
   
       return (
@@ -78,3 +90,22 @@ function Square(props)
     document.getElementById('root')
   );
   
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
