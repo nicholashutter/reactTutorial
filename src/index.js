f7349810bbaf2b27ddd9ad852b1b67ce6f80f794
nicholashutter <nicholas.hutter@live.com>
2021-12-22 18:30:58 -0600

    game ignores input after win condition, final commit

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ function Square(props)
   
     handleClick(i) {
       const squares = this.state.squares.slice();
+      if (calculateWinner(squares)|| squares[i]) {return;}
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({squares: squares, xIsNext: !this.state.xIsNext,});
     }
